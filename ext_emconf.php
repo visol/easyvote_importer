d0ada97a79f33e1206cb38a29c3b4958a38ef979
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2015-03-17 16:15:57 +0000

    [TASK] Add composer.json, update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,9 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'extbase' => '6.0',
-			'fluid' => '6.0',
-			'typo3' => '6.0',
+			'easyvote' => '1.0.0-1.99.99',
 		),
 		'conflicts' => array(
 		),
